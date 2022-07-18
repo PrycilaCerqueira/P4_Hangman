0597ed1dc05cbe5450365f359560a1b3806cad27
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-07-17 23:38:32 -0400

    First version of the IF input (String) is Null, Empty or WhiteSpace verification

--- P4_Hangman/Program.cs
+++ P4_Hangman/Program.cs
@@ -8,7 +8,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
             Console.Write("** Hangman Game **\n\n");
 
-            string word = "";
+            string word = "test";
             List<string> bankOfWords = new List<string>(); //Creating and initializing the list name bankOfWords
             Console.Write($"Create your own back of words. Enter 10 words:\n");
 
@@ -16,7 +16,27 @@ namespace MyApp // Note: actual namespace depends on the project name.
             {
                 Console.Write(@$"{wordCount})");
                 word = Console.ReadLine();
-                bankOfWords.Add(word.ToLower()); //Add the word converted to lower case to the bankOfWords list             
+
+                //TODO: Fix the loop of repeated word verification 
+                /*
+                for (int i = 1; i <= wordCount; i++) //Verify if the word already exist in the list
+                {
+                    if (word == bankOfWords[wordCount])
+                    {
+                        Console.WriteLine("This word already exist. Please, enter a new one here: ");
+                        word = Console.ReadLine();
+                    }
+                }
+                */
+                if (String.IsNullOrEmpty(word) || String.IsNullOrWhiteSpace(word)) //Add a word if not empty, null, or spaces
+                {
+                    Console.WriteLine("Invalidy entry. PLease, try again.");
+                }
+                else
+                {
+                    bankOfWords.Add(word.ToLower()); //Adding the word converted to lower case to the bankOfWords list
+                }
+
             }
 
             Console.WriteLine("\nConfirm your words:");
@@ -24,6 +44,10 @@ namespace MyApp // Note: actual namespace depends on the project name.
             {
                 Console.WriteLine(item);
             }
+
+            //TODO: Add a IF structure for the user confirm wether the list is valid or not.
+
+
             
         }
     }
