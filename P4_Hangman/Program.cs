5e87cb23df212f1d26b3d8debc6e8918ad8826e7
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-07-17 22:37:49 -0400

    Insert items to the list

--- P4_Hangman/Program.cs
+++ P4_Hangman/Program.cs
@@ -2,11 +2,29 @@
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
-    internal class Program
+    internal class P4_Hangman
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("** Hangman Game **\n\n");
+
+            string word = "";
+            List<string> bankOfWords = new List<string>(); //Creating and initializing the list name bankOfWords
+            Console.Write($"Create your own back of words. Enter 10 words:\n");
+
+            for (int wordCount = 1; wordCount <= 5; wordCount++) //Adding words to the list
+            {
+                Console.Write(@$"{wordCount})");
+                word = Console.ReadLine();
+                bankOfWords.Add(word.ToLower());           
+            }
+
+            Console.WriteLine("Confirm your words:");
+            foreach (string item in bankOfWords) //Printing the list items for the player verify the words inserted
+            {
+                Console.WriteLine(item);
+            }
+            
         }
     }
 }
