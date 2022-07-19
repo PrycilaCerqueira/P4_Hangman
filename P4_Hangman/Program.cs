bb5201e69ca270b9648672bcdd5ce1be31d415d1
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-07-18 23:52:31 -0400

    Updated comments

--- P4_Hangman/Program.cs
+++ P4_Hangman/Program.cs
@@ -10,16 +10,16 @@ namespace P4_Hangman // Note: actual namespace depends on the project name.
 
             //BLOCK 1 - List of words creation
             string word = "";
-            List<string> bankOfWords = new List<string>(); //Creating and initializing the list named bankOfWords
+            List<string> bankOfWords = new List<string>(); //Creates and initializes the list named bankOfWords
             Console.Write($"Create your own back of words. Enter 10 words:\n");
 
-            for (int wordCount = 1; wordCount <= 3; wordCount++) //Adding words to the bankOfWords list
+            for (int wordCount = 1; wordCount <= 3; wordCount++) //Add words to the bankOfWords list
             {
                 Console.Write(@$"{wordCount})");
                 word = Console.ReadLine();
 
                 //TODO: How to check for every new entry - Maybe DO/WHILE
-                if (String.IsNullOrEmpty(word) || String.IsNullOrWhiteSpace(word)) //Verify if word is empty, null, or spaces
+                if (String.IsNullOrEmpty(word) || String.IsNullOrWhiteSpace(word)) //Verifies if the word is empty, null, or spaces
                 {
                     Console.WriteLine("Null and spaces are invalidy entries. Please, try again.");
                     Console.Write("Enter a valid word: ");
@@ -27,19 +27,19 @@ namespace P4_Hangman // Note: actual namespace depends on the project name.
                 }
 
                 //TODO: How to check for every new entry - Maybe DO/WHILE
-                if (bankOfWords.Contains(word)) //Verify if word is repeated
+                if (bankOfWords.Contains(word)) //Verifies if the word is repeated
                 {
                     Console.WriteLine("This word already exist in your list.");
                     Console.Write("Enter a new word: ");
                     word = Console.ReadLine();
                 }
 
-                bankOfWords.Add(word.ToLower()); //Adding a word converted to lower case to the list
+                bankOfWords.Add(word.ToLower()); //Adds a lower case converted word to the list
 
              }
 
             Console.WriteLine("\nHere are your saved words:");
-            foreach (string item in bankOfWords) //Printing the bankOfWords items for the player verify the words inserted
+            foreach (string item in bankOfWords) //Print the bankOfWords items for the player
             {
                 Console.WriteLine(item);
             }
@@ -48,12 +48,12 @@ namespace P4_Hangman // Note: actual namespace depends on the project name.
             Console.Write("\nAre you happy with your list? (Yes/No) ");
             confirmation = Console.ReadLine(); 
             
-            if (confirmation.ToLower() != "yes" && confirmation.ToLower() != "no") //Confirming if the player entered a valid confirmation word
+            if (confirmation.ToLower() != "yes" && confirmation.ToLower() != "no") //Confirms if the player entered a valid confirmation word
             {
                 Console.Write("This is an invalid entry. Choose Yes or No to proceed: ");
                 confirmation = Console.ReadLine();
             }
-            if (confirmation.ToLower() == "no") //Confirming if the player would like to proceed in the game
+            if (confirmation.ToLower() == "no") //Confirms if the player would like to proceed in the game
             {
                 Console.Write("Sorry to hear that! See you next time.");
                 Environment.Exit(0); //Exits the console, forcing the game to end 
@@ -66,7 +66,7 @@ namespace P4_Hangman // Note: actual namespace depends on the project name.
 
             Console.WriteLine(getWord);
 
-            //BLOCK 3 - Discover if a string is part of the picked word
+            //BLOCK 3 - Discover if a character is part of the picked word
             //TODO: Try out String.IndexOf( ) and/or String.Contains( )
 
 
