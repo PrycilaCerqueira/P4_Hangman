94ed8b66933bbf6ac9af8a8d7baae18c7b0c0d72
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-07-21 23:31:53 -0400

    Built a first functional String Loop

--- P4_Hangman/Program.cs
+++ P4_Hangman/Program.cs
@@ -47,35 +47,51 @@ namespace P4_Hangman // Note: actual namespace depends on the project name.
             }
 
             string confirmation = "";
-            Console.Write("\nAre you happy with your list? (Yes/No) ");
+            Console.Write("\nAre you happy with your list? (Y/N) ");
             confirmation = Console.ReadLine(); 
             
-            while (confirmation.ToLower() != "yes" && confirmation.ToLower() != "no") //Confirms if the player entered a valid confirmation word
+            while (confirmation.ToLower() != "y" && confirmation.ToLower() != "n") //Confirms if the player entered a valid confirmation word
             {
                 Console.Write("This is an invalid entry. Choose Yes or No to proceed: ");
                 confirmation = Console.ReadLine();
             }
-            if (confirmation.ToLower() == "no") //Confirms if the player would like to proceed in the game
+            if (confirmation.ToLower() == "n") //Confirms if the player would like to proceed in the game
             {
                 Console.Write("Sorry to hear that! See you next time.");
                 Environment.Exit(0); //Exits the console, forcing the game to end 
             }
 
             //BLOCK 2 - Pick a random word from the list
-            Random randomWord = new Random();
-            int wordIndex = randomWord.Next(bankOfWords.Count);
+            Random rnd = new Random();
+            int wordIndex = rnd.Next(bankOfWords.Count);
             string pickedWord = bankOfWords[wordIndex];
 
             Console.WriteLine(pickedWord);
 
             //BLOCK 3 - Discover if a character is part of the picked word
-            //TODO: Try out String.IndexOf( ) and/or String.Contains( )
+            //TODO: Loop the guessedLetter index through the pickedWord 
             string guessedLetter = "";
-            Console.Write("Enter a letter: ");
+            Console.Write("Guess a letter: ");
             guessedLetter = Console.ReadLine();
-            guessedLetter = guessedLetter.ToLower();
-            Console.WriteLine(pickedWord.Contains(guessedLetter));
-            Console.WriteLine(pickedWord.IndexOf(guessedLetter));
+            
+            char[] wordArray = pickedWord.ToCharArray();
+
+            //pickedWord.IndexOf(guessedLetter);
+            if (pickedWord.Contains(guessedLetter.ToLower()))
+            {
+                for (int i = 0; i < wordArray.Length; i++)
+                {
+                    if (guessedLetter == pickedWord[i].ToString())
+                    {
+                        Console.Write(wordArray[i]);
+                    }
+                    
+                }
+            }
+
+            
+
+            //BLOCK 4 - Draw the shape of the hangman
 
         }
     }
